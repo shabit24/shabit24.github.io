3f8a00c07d227a504137369ca9244fe479261b51
shabit24 <127089101+shabit24@users.noreply.github.com>
2024-04-02 09:01:13 +0700

    done
    test

---
+++ script.js
@@ -0,0 +1,15 @@
+function showAlert(){
+    alert(Date(), tampil(), show())
+    Date()
+
+    let tanggal = new Date();
+    document.getElementById("").innerHTML = tanggal;
+
+    function tampil(){
+        alert("The term edelweiss comes from German which means start and color")
+    }
+
+    function show(){
+        alert("The white color symbolizes the national era")
+    }
+}
